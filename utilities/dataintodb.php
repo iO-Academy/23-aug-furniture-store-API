95fb4730bb803f6c1ec890f3c0489b6455ac04c7
Mitch <michaelthierytucker@gmail.com>
2023-11-06 15:20:05 +0000

    removed blank space

--- utilities/dataintodb.php
+++ utilities/dataintodb.php
@@ -14,15 +14,9 @@ $decodedData = json_decode($rawData);
 $categories =
     [1 => 'Chair', 2 => 'Office Chair', 3 => 'Book case', 4 => 'Table', 5 => 'Draws', 6 => 'Wardrobe', 7 => 'Chest', 8 => 'TV Stand', 9 => 'Shelves', 10 => 'Desk', 11 => 'Sofa'];
 
-
 foreach ($decodedData as $value) {
     $categoryId = array_search($value->name, $categories);
     $price = ltrim($value->price, '£');
     $query = $pdo->prepare("INSERT INTO `products` (`category_ID`, `width`, `height`, `depth`, `price`, `stock`, `related`, `color`) VALUES ($categoryId, $value->width, $value->height, $value->depth, $price, $value->stock, $value->related, ?);");
     $query->execute([$value->color]);
 }
-echo '<pre>';
-print_r($categories);
-echo '</pre';
-
-
