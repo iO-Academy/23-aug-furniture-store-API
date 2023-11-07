6472107b31e3a83cfbf5d2db69bd907875792b8a
Mitch <michaelthierytucker@gmail.com>
2023-11-07 14:06:42 +0000

    products and Exceptions

--- products.php
+++ products.php
@@ -7,16 +7,22 @@ require('vendor/autoload.php');
 use Furniture\Factories\DbConnector;
 use Furniture\Hydrators\ProductHydrator;
 use Furniture\Services\ResponseService;
+use \Furniture\Exceptions\InvalidCategoryException;
 
 const SUCCESS_MESSAGE = "Successfully retrieved products";
 
 try {
+    if (!is_numeric($_GET['cat'])) {
+        throw new InvalidCategoryException('INVALID CATEGORY ID');
+    }
     $db = DbConnector::getDbConnection();
-    $products = ProductHydrator::fetchProductsByCategoryId($db, 1);
+    $products = ProductHydrator::fetchProductsByCategoryId($db, $_GET['cat']);
     if (empty($products)) {
         throw new Exception('No products found in database');
     }
     echo json_encode(ResponseService::createResponse(SUCCESS_MESSAGE, $products));
+} catch (InvalidCategoryException $invalidCatEx) {
+    echo json_encode(ResponseService::createResponse(ResponseService::INVALID_CAT, [], 400));
 } catch (Exception $exception) {
     echo json_encode(ResponseService::createResponse(ResponseService::UNEXPECTED_ERROR, [], 500));
 }
