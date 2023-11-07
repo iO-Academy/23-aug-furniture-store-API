74e9c1b0bf82b3ea4a8a1271942d6ad528139a8a
Jake <jake_lewis12@hotmail.co.uk>
2023-11-07 15:29:02 +0000

    added outline to detailed product entity

--- src/Entities/DetailedProductEntity.php
+++ src/Entities/DetailedProductEntity.php
@@ -2,7 +2,17 @@
 
 namespace Furniture\Entities;
 
-class DetailedProductEntity
+class DetailedProductEntity extends ProductEntity
 {
+    private int $categoryId;
+    private int $width;
+    private int $height;
+    private int $depth;
+    private int $related;
+
+    public function jsonSerialize(): array
+    {
+        return ['categoryId' => $this->categoryId, 'width' => $this->width, 'height' => $this->height, 'depth' => $this->depth, 'related' => $this->related];
+    }
 
 }
