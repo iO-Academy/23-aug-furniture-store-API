28a1b3e742d5958ccd5d5205f5eb3a86e4faccdc
Jake <jake_lewis12@hotmail.co.uk>
2023-11-08 10:32:54 +0000

    added invalid unit exception to product.php

--- product.php
+++ product.php
@@ -6,15 +6,21 @@ use Furniture\Hydrators\ProductHydrator;
 use Furniture\Services\ResponseService;
 use \Furniture\Exceptions\InvalidProductException;
 use \Furniture\Entities\DetailedProductEntity;
+use \Furniture\Exceptions\InvalidUnitException;
 
 const SUCCESS_MESSAGE = "Successfully retrieved product";
 
 $productId = $_GET['id'] ?? '';
-$unit = $_GET['unit'] ?? '';
+$unit = $_GET['unit'] ?? 'mm';
+$validUnits = ['mm','cm','in','ft'];
+//$validUnits = MeasurementConverterService::Valid_UNITS;
 
 try {
     if (!is_numeric($productId)) {
         throw new InvalidProductException(InvalidProductException::INVALID_PROD_ID);
+    }
+    if (!in_array($unit, $validUnits)) {
+        throw new InvalidUnitException(InvalidUnitException::INVALID_UNIT);
     }
     $db = DbConnector::getDbConnection();
     $product= ProductHydrator::fetchProductById($db, $productId);
@@ -25,6 +31,8 @@ try {
     $response = json_encode(ResponseService::createResponse(SUCCESS_MESSAGE, $product));
 } catch (InvalidProductException $invalidProdEx) {
     $response = json_encode(ResponseService::createResponse($invalidProdEx->getMessage(), [], 400));
+} catch (InvalidUnitException $invalidUnitEx) {
+    $response = json_encode(ResponseService::createResponse($invalidUnitEx->getMessage(), [], 400));
 } catch (Exception $exception) {
     $response = json_encode(ResponseService::createResponse(ResponseService::UNEXPECTED_ERROR, [], 500));
 }
