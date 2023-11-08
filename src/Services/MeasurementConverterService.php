11c3f3511febc329dff0016c30697e3fbeb6aa7d
Tiago <tiagovarela01@hotmail.com>
2023-11-08 09:54:23 +0000

    Measurement Coverter Service added

---
+++ src/Services/MeasurementConverterService.php
@@ -0,0 +1,30 @@
+<?php
+
+class MeasurementConverterService
+{
+    const VALID_UNITS = ['mm', 'cm', 'in', 'ft'];
+    const MM = 1;
+    const CM = 10;
+    const IN = 25.4;
+    const FT = 304.8;
+
+    public static function convertMeasurement(string $unit, int $value):float
+    {
+
+        switch ($unit){
+            case 'cm':
+                $unitConversion = self::CM;
+                break;
+            case 'in':
+                $unitConversion = self::IN;
+                break;
+            case 'ft':
+                $unitConversion = self::FT;
+                break;
+            default:
+                $unitConversion = self::MM;
+        }
+       return $value / $unitConversion;
+    }
+}
+
